bd6ff6c109d60151abb5fe465976f858cbb1bb4d
Pietrzi <pietrzi@op.pl>
2020-06-17 13:11:59 +0200

    finally can code along

--- users-app/routes/api/profile.js
+++ users-app/routes/api/profile.js
@@ -115,7 +115,7 @@ router.post(
       // profileFields.social = socialfields;
 
       try {
-        // Using upsert option (creates new doc if no match is found):
+        
         let profile = await Profile.findOne({ user: req.user.id });
 
         if(profile) {
@@ -139,6 +139,8 @@ router.post(
         res.status(500).send('Server Error');
       }
   
+      // version with upsert
+
       // try {
       //   // Using upsert option (creates new doc if no match is found):
       //   let profile = await Profile.findOneAndUpdate(
@@ -155,59 +157,59 @@ router.post(
   );
 
 
-//   // @route    GET api/profile
-// // @desc     Get all profiles
-// // @access   Public
-// router.get('/', async (req, res) => {
-//   try {
-//     const profiles = await Profile.find().populate('user', ['name', 'avatar']);
-//     res.json(profiles);
-//   } catch (err) {
-//     console.error(err.message);
-//     res.status(500).send('Server Error');
-//   }
-// });
+  // @route    GET api/profile
+// @desc     Get all profiles
+// @access   Public
+router.get('/', async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+    res.json(profiles);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
-// // @route    GET api/profile/user/:user_id
-// // @desc     Get profile by user ID
-// // @access   Public
-// router.get(
-//   '/user/:user_id',
-//   checkObjectId('user_id'),
-//   async ({ params: { user_id } }, res) => {
-//     try {
-//       const profile = await Profile.findOne({
-//         user: user_id
-//       }).populate('user', ['name', 'avatar']);
+// @route    GET api/profile/user/:user_id
+// @desc     Get profile by user ID
+// @access   Public
+router.get(
+  '/user/:user_id',
+  checkObjectId('user_id'),
+  async ({ params: { user_id } }, res) => {
+    try {
+      const profile = await Profile.findOne({
+        user: user_id
+      }).populate('user', ['name', 'avatar']);
 
-//       if (!profile) return res.status(400).json({ msg: 'Profile not found' });
+      if (!profile) return res.status(400).json({ msg: 'Profile not found' });
 
-//       return res.json(profile);
-//     } catch (err) {
-//       console.error(err.message);
-//       return res.status(500).json({ msg: 'Server error' });
-//     }
-//   }
-// );
+      return res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      return res.status(500).json({ msg: 'Server error' });
+    }
+  }
+);
 
-// // @route    DELETE api/profile
-// // @desc     Delete profile, user & posts
-// // @access   Private
-// router.delete('/', auth, async (req, res) => {
-//   try {
-//     // Remove user posts
-//     await Post.deleteMany({ user: req.user.id });
-//     // Remove profile
-//     await Profile.findOneAndRemove({ user: req.user.id });
-//     // Remove user
-//     await User.findOneAndRemove({ _id: req.user.id });
+// @route    DELETE api/profile
+// @desc     Delete profile, user & posts
+// @access   Private
+router.delete('/', auth, async (req, res) => {
+  try {
+    // Remove user posts
+    await Post.deleteMany({ user: req.user.id });
+    // Remove profile
+    await Profile.findOneAndRemove({ user: req.user.id });
+    // Remove user
+    await User.findOneAndRemove({ _id: req.user.id });
 
-//     res.json({ msg: 'User deleted' });
-//   } catch (err) {
-//     console.error(err.message);
-//     res.status(500).send('Server Error');
-//   }
-// });
+    res.json({ msg: 'User deleted' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 
   module.exports = router;
